eb6cc8f2eebd3e6fff5d26e545fb73b2083a60a7
rebch86 <rebch86@daum.net>
2020-08-18 17:47:43 +0900

    예제코드 수정

--- react-hooks/src/hooks/useHook.js
+++ react-hooks/src/hooks/useHook.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react';
 
+// 사용자 정보를 받는 단순한 커스텀 훅(API 통신과 연동해서 사용가능)
 export default function useHook() {
     const [age, setAge] = useState(0);
     const [name, setName] = useState('야스');
--- react-hooks/src/hooks/useMounted.js
+++ react-hooks/src/hooks/useMounted.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react';
 
+// 컴포넌트가 렌더링 결과가 실제 DOM 에 반영된(mounted) 상태인지 검사
 export default function useMounted() {
     const [mounted, setMounted] = useState(false);
 
